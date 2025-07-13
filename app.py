69d5d53b6fc54c8ee3378c757a02ecb4b7208597
devyani1512 <148792031+devyani1512@users.noreply.github.com>
2025-07-13 17:44:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def ask():
     return jsonify({"items": items, "reply": reply})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)), debug=True)
+
